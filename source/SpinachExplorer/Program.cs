582137138078c2c29b77f9b1b89b354d3248332a
Kevin Dietz <kdietz65@gmail.com>
2023-06-27 01:55:23 -0500

    Index files, improve performance with LRU cache

--- source/SpinachExplorer/Program.cs
+++ source/SpinachExplorer/Program.cs
@@ -3,6 +3,9 @@ using Eugene.Blocks;
 using Eugene.Collections;
 using Spinach.Trigrams;
 using SpinachExplorer;
+using SpinachExplorer.Caching;
+using System.Threading.Channels;
+using System.Xml;
 
 internal static class Program
 {
@@ -13,6 +16,39 @@ internal static class Program
   static Program()
   {
     DiskBlockManager = new DiskBlockManager();
+    TrigramKeyType = DiskBlockManager.RegisterBlockType<TrigramKey>();
+    
+    // The FileIdTree is used to look up file names based on their internal id
+    // Given a key of internal file id, it returns an address to a DiskImmutableString
+    FileIdTreeFactory =
+      DiskBlockManager.BTreeManager.CreateFactory<long, long>(
+        DiskBlockManager.LongBlockType,
+        DiskBlockManager.LongBlockType
+      );
+
+    // The TrigramTree is used to look up a trigram and figure out which TrigramFileTree goes with it.
+    // Given a key representing a trigram, it returns an address a a TrigramFileTree BTree
+    TrigramTreeFactory =
+      DiskBlockManager.BTreeManager.CreateFactory<int, long>(
+        DiskBlockManager.IntBlockType,
+        DiskBlockManager.LongBlockType
+      );
+    
+    // The TrigramFileTree is used to look up a file for a given trigram. Each individual trigram
+    // corresponds to a BTree of file ids that contain that trigram. Given a key of an internal
+    // file id, it returns an address to a DiskLinkedList<long> which is a list that contains the
+    // position(s) where that trigram appears in the file.
+    TrigramFileTreeFactory =
+      DiskBlockManager.BTreeManager.CreateFactory<long, long>(
+        DiskBlockManager.LongBlockType,
+        DiskBlockManager.LongBlockType
+      );
+
+    LinkedListOfLongFactory =
+      DiskBlockManager.LinkedListManager.CreateFactory<long>(DiskBlockManager.LongBlockType);
+
+    TrigramFileIdTreeCache = new LruCache<int, DiskBTree<long, long>>(2200000);
+    PostingsListCache = new LruCache<Tuple<int, long>, DiskLinkedList<long>>(2200000);
   }
 
   // /////////////////////////////////////////////////////////////////////////////////////////////
@@ -42,6 +78,7 @@ internal static class Program
       Console.WriteLine("5. Index local files");
       Console.WriteLine("6. Lookup file id");
       Console.WriteLine("7. Test trigram extractor");
+      Console.WriteLine("8. Index files");
       Console.WriteLine("X. Exit program");
       Console.WriteLine();
       Console.Write("Enter selection: ");
@@ -79,6 +116,10 @@ internal static class Program
           case "7":
             TestTrigramExtractor();
             break;
+          
+          case "8":
+            IndexFiles();
+            break;
 
           case "x":
             finished = true;
@@ -105,7 +146,21 @@ internal static class Program
 
   private static DiskBTree<long, long> InternalFileIdTree { get; set; }
 
-  private static DiskBTree<TrigramKey, short> TrigramTree { get; set; }
+  private static DiskBTree<int, long> TrigramTree { get; set; }
+
+  private static short TrigramKeyType { get; set; }
+  
+  private static DiskBTreeFactory<long, long> FileIdTreeFactory { get; set; }
+  
+  private static DiskBTreeFactory<int, long> TrigramTreeFactory { get; set; }
+  
+  private static DiskBTreeFactory<long, long> TrigramFileTreeFactory { get; set; }
+  
+  private static DiskLinkedListFactory<long> LinkedListOfLongFactory { get; set; }
+  
+  private static LruCache<int, DiskBTree<long, long>> TrigramFileIdTreeCache { get; set; }
+  
+  private static LruCache<Tuple<int, long>, DiskLinkedList<long>> PostingsListCache { get; set; }
 
   // /////////////////////////////////////////////////////////////////////////////////////////////
   // Private Static Methods
@@ -154,20 +209,17 @@ internal static class Program
     DiskBlockManager.Close();
     DiskBlockManager.CreateOrOpen(filename);
 
-    DiskBTreeFactory<long, long> fileIdTreeFactory =
-      DiskBlockManager.BTreeManager.CreateFactory<long, long>(
-        DiskBlockManager.LongBlockType,
-        DiskBlockManager.LongBlockType
-      );
-
-    InternalFileIdTree = fileIdTreeFactory.AppendNew();
+    InternalFileIdTree = FileIdTreeFactory.AppendNew(100);
+    TrigramTree = TrigramTreeFactory.AppendNew(100);
 
     HeaderBlock headerBlock = DiskBlockManager.GetHeaderBlock();
     headerBlock.Address1 = InternalFileIdTree.Address;
+    headerBlock.Address2 = TrigramTree.Address;
 
     DiskBlockManager.WriteHeaderBlock(ref headerBlock);
 
     Console.WriteLine($"InternalFileIdTree Stored at Address: {headerBlock.Address1}");
+    Console.WriteLine($"TrigramTree Stored at Address: {headerBlock.Address2}");
 
     IsOpen = true;
     FileName = filename;
@@ -206,15 +258,11 @@ internal static class Program
 
     HeaderBlock headerBlock = DiskBlockManager.GetHeaderBlock();
 
-    DiskBTreeFactory<long, long> fileIdTreeFactory =
-      DiskBlockManager.BTreeManager.CreateFactory<long, long>(
-        DiskBlockManager.LongBlockType,
-        DiskBlockManager.LongBlockType
-      );
-
-    InternalFileIdTree = new DiskBTree<long, long>(fileIdTreeFactory, headerBlock.Address1);
+    InternalFileIdTree = FileIdTreeFactory.LoadExisting(headerBlock.Address1);
+    TrigramTree = TrigramTreeFactory.LoadExisting(headerBlock.Address2);
 
     Console.WriteLine($"InternalFileIdTree Loaded from Address: {headerBlock.Address1}");
+    Console.WriteLine($"TrigramTree Loaded from Address: {headerBlock.Address2}");
 
     IsOpen = true;
     FileName = filename;
@@ -248,7 +296,7 @@ internal static class Program
 
     Console.WriteLine("Indexing files ...");
 
-    foreach (string filePath in Directory.GetFiles("/home/kdietz/dev/OpenSquiggly", "*.*", SearchOption.AllDirectories))
+    foreach (string filePath in Directory.GetFiles("/home/kdietz/spinach-repos/OpenSquiggly", "*.*", SearchOption.AllDirectories))
     {
       Console.WriteLine($"{currentFileId} : {filePath}");
       DiskImmutableString nameString = DiskBlockManager.ImmutableStringFactory.Append(filePath);
@@ -262,14 +310,21 @@ internal static class Program
   private static void LookupFileId()
   {
     Console.WriteLine();
-    Console.Write("Enter internal file id: ");
-    string response = Console.ReadLine();
-    if (int.TryParse(response, out int responseVal))
+    
+    Console.Write("Enter first internal file id: ");
+    string firstResponse = Console.ReadLine();
+    
+    Console.Write("Enter last internal file id: ");
+    string lastResponse = Console.ReadLine();
+    
+    if (int.TryParse(firstResponse, out int firstFileId) && int.TryParse(lastResponse, out int lastFileId))
     {
-      long nameAddress = InternalFileIdTree.Find(responseVal);
-      Console.WriteLine($"Name Address = {nameAddress}");
-      DiskImmutableString nameString = DiskBlockManager.ImmutableStringFactory.LoadExisting(nameAddress);
-      Console.WriteLine($"Name = {nameString.GetValue()}");
+      for (int fileId = firstFileId; fileId <= lastFileId; fileId++)
+      {
+        long nameAddress = InternalFileIdTree.Find(fileId);
+        DiskImmutableString nameString = DiskBlockManager.ImmutableStringFactory.LoadExisting(nameAddress);
+        Console.WriteLine($"{fileId}: {nameString.GetValue()}");
+      }
     }
     else
     {
@@ -279,24 +334,266 @@ internal static class Program
     Pause();
   }
 
+  private static bool FileIsBinary(string filename)
+  {
+    var buffer = new byte[8192];
+
+    using (var fileStream = File.OpenRead(filename))
+    {
+      var bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+        
+      for (int i = 0; i < bytesRead; i++)
+      {
+        if (buffer[i] < 0x09)
+        {
+          return true; // File contains a null byte or other control character; assume binary
+        }
+      }
+    }
+    
+    return false;     
+  }
+
   private static void TestTrigramExtractor()
   {
+    // Console.WriteLine();
+    // Console.Write("Enter a string: ");
+    // string response = Console.ReadLine();
+    
     Console.WriteLine();
-    Console.Write("Enter a string: ");
-    string response = Console.ReadLine();
+    
+    Console.Write("Enter first internal file id: ");
+    string firstResponse = Console.ReadLine();
+    
+    Console.Write("Enter last internal file id: ");
+    string lastResponse = Console.ReadLine();
 
-    var trigramExtractor = new TrigramExtractor(response);
-
-    foreach (TrigramInfo trigramInfo in trigramExtractor)
+    if (int.TryParse(firstResponse, out int firstFileId) && int.TryParse(lastResponse, out int lastFileId))
     {
-      char ch1 = (char)(trigramInfo.Key / 128L / 128L);
-      char ch2 = (char)(trigramInfo.Key % (128L * 128L) / 128L);
-      char ch3 = (char)(trigramInfo.Key % 128L);
+      for (int fileId = firstFileId; fileId <= lastFileId; fileId++)
+      {
+        long nameAddress = InternalFileIdTree.Find(fileId);
+        DiskImmutableString nameString = DiskBlockManager.ImmutableStringFactory.LoadExisting(nameAddress);
+        string name = nameString.GetValue();
 
-      Console.WriteLine($"Key = {trigramInfo.Key}  Trigram = '{ch1}{ch2}{ch3}'  Position = {trigramInfo.Position}");
+        if (name.Contains("/.git/"))
+        {
+          Console.Write(".");
+          continue;
+        }
+        
+        if (name.Contains("/obj/"))
+        {
+          Console.Write(".");
+          continue;
+        }
+        
+        if (name.Contains("/bin/"))
+        {
+          Console.Write(".");
+          continue;
+        }        
+
+        if (name.Contains("node_modules"))
+        {
+          Console.Write(".");
+          continue;
+        }
+
+        if (name.EndsWith(".jpg") || name.EndsWith(".gif") || name.EndsWith(".png") || name.EndsWith(".dll"))
+        {
+          Console.Write(".");
+          continue;
+        }
+
+        if (FileIsBinary(name))
+        {
+          Console.Write(".");
+          continue;
+        }
+
+        string content = File.ReadAllText(name);
+        
+        var trigramExtractor = new TrigramExtractor(content);
+        int count = 0;
+        
+        foreach (TrigramInfo trigramInfo in trigramExtractor)
+        {
+          char ch1 = (char)(trigramInfo.Key / 128L / 128L);
+          char ch2 = (char)(trigramInfo.Key % (128L * 128L) / 128L);
+          char ch3 = (char)(trigramInfo.Key % 128L);
+          count++;
+          Console.Write(".");
+
+          // Console.WriteLine($"Key = {trigramInfo.Key}  Position = {trigramInfo.Position}");
+        } 
+        
+        Console.WriteLine($"{fileId} : {count} : {nameString.GetValue()}");
+        // Console.WriteLine($"{fileId} : {count}");
+      }
     }
 
     Pause();
   }
+
+  private static DiskBTree<long, long> LoadOrAddTrigramFileIdTree(int trigramKey, long fileId, out bool created)
+  {
+    created = false;
+    
+    if (TrigramFileIdTreeCache.TryGetValue(trigramKey, out DiskBTree<long, long> btree))
+    {
+      return btree;
+    }
+    
+    if (TrigramTree.TryFind(trigramKey, out long trigramFileIdTreeAddress))
+    {
+      DiskBTree<long, long> trigramFileIdTree = 
+        TrigramFileTreeFactory.LoadExisting(trigramFileIdTreeAddress);
+
+      TrigramFileIdTreeCache.Add(trigramKey, trigramFileIdTree);
+      return trigramFileIdTree;
+    }
+    
+    DiskBTree<long, long> newTrigramFileIdTree = TrigramFileTreeFactory.AppendNew(100);
+    DiskLinkedList<long> linkedList = LinkedListOfLongFactory.AppendNew();
+    newTrigramFileIdTree.Insert(fileId, linkedList.Address);
+    TrigramTree.Insert(trigramKey, newTrigramFileIdTree.Address);
+    TrigramFileIdTreeCache.Add(trigramKey, newTrigramFileIdTree);
+    created = true;
+
+    return newTrigramFileIdTree;
+  }
+
+  private static DiskLinkedList<long> LoadOrAddPostingsList(int trigramKey, long fileId)
+  {
+    var key = new Tuple<int, long>(trigramKey, fileId);
+
+    if (PostingsListCache.TryGetValue(key, out DiskLinkedList<long> postingsList))
+    {
+      return postingsList;
+    }
+
+    DiskBTree<long, long> trigramFileIdTree = LoadOrAddTrigramFileIdTree(trigramKey, fileId, out bool created);
+    if (!created)
+    {
+      if (!trigramFileIdTree.TryFind(fileId, out long postingsListAddress))
+      {
+        DiskLinkedList<long> newPostingsList1 = LinkedListOfLongFactory.AppendNew();
+        trigramFileIdTree.Insert(fileId, newPostingsList1.Address);
+        PostingsListCache.Add(key, newPostingsList1);
+
+        return newPostingsList1;
+      }
+
+      DiskLinkedList<long> diskPostingsList = LinkedListOfLongFactory.LoadExisting(postingsListAddress);
+      PostingsListCache.Add(key, diskPostingsList);
+
+      return diskPostingsList;
+    }
+
+    DiskLinkedList<long> newPostingsList2 = LinkedListOfLongFactory.AppendNew();
+    trigramFileIdTree.Insert(fileId, newPostingsList2.Address);
+    PostingsListCache.Add(key, newPostingsList2);
+
+    return newPostingsList2;
+  }
+  
+  private static void IndexFiles()
+  {
+    Console.WriteLine();
+    
+    Console.Write("Enter first internal file id: ");
+    string firstResponse = Console.ReadLine();
+    
+    Console.Write("Enter last internal file id: ");
+    string lastResponse = Console.ReadLine();
+
+    if (int.TryParse(firstResponse, out int firstFileId) && int.TryParse(lastResponse, out int lastFileId))
+    {
+      for (int fileId = firstFileId; fileId <= lastFileId; fileId++)
+      {
+        long nameAddress = InternalFileIdTree.Find(fileId);
+        DiskImmutableString nameString = DiskBlockManager.ImmutableStringFactory.LoadExisting(nameAddress);
+        string name = nameString.GetValue();
+
+        if (name.Contains("/.git/"))
+        {
+          continue;
+        }
+        
+        if (name.Contains("/obj/"))
+        {
+          continue;
+        }
+        
+        if (name.Contains("/bin/"))
+        {
+          continue;
+        }        
+
+        if (name.Contains("node_modules"))
+        {
+          continue;
+        }
+
+        if (name.EndsWith(".jpg") || name.EndsWith(".gif") || name.EndsWith(".png") || name.EndsWith(".dll"))
+        {
+          continue;
+        }
+
+        if (FileIsBinary(name))
+        {
+          continue;
+        }
+
+        Console.Write($"Indexing {fileId} : {name} ...");
+
+        string content = File.ReadAllText(name);
+        
+        var trigramExtractor = new TrigramExtractor(content);
+        int count = 0;
+
+        foreach (TrigramInfo trigramInfo in trigramExtractor)
+        {
+          char ch1 = (char)(trigramInfo.Key / 128L / 128L);
+          char ch2 = (char)(trigramInfo.Key % (128L * 128L) / 128L);
+          char ch3 = (char)(trigramInfo.Key % 128L);
+
+          DiskLinkedList<long> postingsList = LoadOrAddPostingsList(trigramInfo.Key, fileId);
+          postingsList.AddLast(trigramInfo.Position);
+
+          // if (!TrigramTree.TryFind(trigramInfo.Key, out long trigramFileIdTreeAddress))
+          // {
+          //   DiskBTree<long, long> trigramFileIdTree = TrigramFileTreeFactory.AppendNew(100);
+          //   var linkedList = LinkedListOfLongFactory.AppendNew();
+          //   trigramFileIdTree.Insert(fileId, linkedList.Address);
+          //   TrigramTree.Insert(trigramInfo.Key, trigramFileIdTree.Address);
+          //   linkedList.AddLast(trigramInfo.Position);
+          // }
+          // else
+          // {
+          //   var trigramFileIdTree = TrigramFileTreeFactory.LoadExisting(trigramFileIdTreeAddress);
+          //   if (trigramFileIdTree.TryFind(fileId, out long linkedListAddress))
+          //   {
+          //     var linkedList = LinkedListOfLongFactory.LoadExisting(linkedListAddress);
+          //     linkedList.AddLast(trigramInfo.Position);
+          //   }
+          //   else
+          //   {
+          //     var linkedList = LinkedListOfLongFactory.AppendNew();
+          //     trigramFileIdTree.Insert(fileId, linkedList.Address);
+          //     linkedList.AddLast(trigramInfo.Position);
+          //   }
+          // }
+
+          count++;
+        } 
+        
+        Console.WriteLine();
+      }
+    }
+
+    Pause();
+  }  
 }
 
