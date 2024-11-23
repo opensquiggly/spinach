efb5e1e9264ff256b4b6797b5b28586742cfd598
Kevin Dietz <kdietz65@gmail.com>
2024-11-23 16:32:41 -0600

    Add IndexFilesForSliceOfTime

--- source/Spinach/Interfaces/ITextSearchManager.cs
+++ source/Spinach/Interfaces/ITextSearchManager.cs
@@ -3,6 +3,7 @@ namespace Spinach.Interfaces;
 public interface ITextSearchManager
 {
   string LoadString(long address);
+  bool IndexFilesForSliceOfTime(CancellationToken cancellationToken, int milliseconds = 5000);
 
   ITextSearchOptions Options { get; }
 }
